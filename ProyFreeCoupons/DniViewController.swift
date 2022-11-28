c6c76cb664d7da859155bb695b8664904445fd51
Douglas Andreus Tafur Saldivar <dtafusal@emeal.nttdata.com>
2022-11-28 15:02:53 -0500

    Creación DniViewController

---
+++ ProyFreeCoupons/DniViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DniViewController.swift
+//  ProyFreeCoupons
+//
+//  Created by Douglas Andreus Tafur Saldivar on 28/11/22.
+//
+
+import UIKit
+
+class DniViewController: UIViewController {
+
+    @IBAction func ObtenerOferta(_ sender: UIButton) {
+        performSegue(withIdentifier: "obtenerQr", sender: nil)
+    }
+    
+    
+    @IBAction func RegresarLista(_ sender: UIButton) {
+        performSegue(withIdentifier: "regresarLista", sender: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
